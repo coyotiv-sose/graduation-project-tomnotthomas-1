44fd3819b69014c58971a36f3f750b7e5e7039d5
tomnotthomas <126760913+tomnotthomas@users.noreply.github.com>
2023-03-24 19:40:33 +0000

    Deleted DateRangePicker and Notification, to focus on uncertain complexity first.

--- src/DateRangePicker.js
+++
@@ -1,7 +0,0 @@
-class DateRangePicker {
-  constructor(startDateforNotification, startDateForCostOptimizationRun) {
-    this.startDateforNotification = startDateforNotification
-    this.startDateForCostOptimizationRun = startDateForCostOptimizationRun
-  }
-}
-module.exports = DateRangePicker
--- src/notification.js
+++
@@ -1,7 +0,0 @@
-class Notification {
-  constructor(notificationforEngineers) {
-    this.notificationforEngineers = notificationforEngineers
-  }
-}
-
-module.exports = Notification
