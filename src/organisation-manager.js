096acf6ac6e90bad9194a06fedfb31ff45457d0f
tomnotthomas <126760913+tomnotthomas@users.noreply.github.com>
2023-03-27 20:26:37 +0000

    added usecase and objects in plantUML

---
+++ src/organisation-manager.js
@@ -0,0 +1,13 @@
+//with this function I want to add an organisation.
+
+let newOrganisation = []
+
+function addOrganisation(organisation) {
+  const organisation = new Organisation(name, owner, id)
+  newOrganisation.push(organisation)
+}
+
+module.exports = {
+  newOrganisation,
+  addOrganisation,
+}
